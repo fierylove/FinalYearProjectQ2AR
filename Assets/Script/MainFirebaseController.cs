833e8a7e391ec2c1b8dcb6d592eba1141488e4ed
thunderblader <thunderblader@live.com>
2018-06-04 11:57:36 +0800

    fixed firebase error

--- Assets/Script/MainFirebaseController.cs
+++ Assets/Script/MainFirebaseController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase;
+using Firebase.Auth;
 
 public class MainFirebaseController : MonoBehaviour {
 
@@ -11,29 +13,29 @@ public class MainFirebaseController : MonoBehaviour {
 
 	// Use this for initialization
 	private void Start () {
-        auth = Firebase.Auth.FirebaseAuth.Defaultinstance;
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
         RegisterNewAccount();
 	}
 	
-	public void egisterNewAccount()
+	public void RegisterNewAccount()
     {
         auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
         {
-            if(task.isCanceled)
+            if(task.IsCanceled)
             {
                 Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled");
                 return;
             }
-            if(task.isFaulted)
+            if(task.IsFaulted)
             {
                 Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
                 return;
             }
 
             Firebase.Auth.FirebaseUser newUser = task.Result;
-            debug.LogFormat("Firebase user created successfully: {0} ({1})", newUser.DisplayName, newUser.UserId);
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})", newUser.DisplayName, newUser.UserId);
 
-            newUser.SendEmailVerificationAsync().ContinueWith(task => {
+            newUser.SendEmailVerificationAsync().ContinueWith(t => {
                 Debug.Log("Verification Email Sent");
             });
         });
